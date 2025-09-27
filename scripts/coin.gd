73688f79a6ba0ed2525d69d46ab7e7b33fbe98c6
Ugoeze-hub <eluchieugoeze@gmail.com>
2025-09-27 14:11:12 +0100

    initial commit

---
+++ scripts/coin.gd
@@ -0,0 +1,9 @@
+extends Area2D
+
+@onready var animation_player: AnimationPlayer = $AnimationPlayer2
+@onready var game_manager: Node = %"Game Manager"
+
+func _on_body_entered(body: Node2D) -> void:
+	game_manager.add_point()
+	animation_player.play("pickup")
+	
